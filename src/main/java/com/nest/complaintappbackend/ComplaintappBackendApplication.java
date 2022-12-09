81dd6e0fb6dfd4521a77062cfc58d4e789c0a326
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-09 14:20:02 +0530

    first commit

---
+++ src/main/java/com/nest/complaintappbackend/ComplaintappBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nest.complaintappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComplaintappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComplaintappBackendApplication.class, args);
+	}
+
+}
